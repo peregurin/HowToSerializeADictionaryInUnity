0c45f56052f6e9a2679a035ceb38557122cba4ab
peregurin <chinmayvirmani@gmail.com>
2021-01-30 15:25:53 +0530

    dictionary made visible inside the inspector
    Used custom editor to change values in the dictionary, save them and print them.

---
+++ Assets/Editor/DictionaryScriptEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DictionaryScript))]
+public class DictionaryScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (((DictionaryScript)target).modifyValues)
+        {
+            if(GUILayout.Button("Save Changes"))
+            {
+                ((DictionaryScript)target).DeserializeDictionary();
+            }
+        }
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if(GUILayout.Button("Print Dictionary"))
+        {
+            ((DictionaryScript)target).PrintDictionary();
+        }
+    }
+}
--- Assets/Scripts/DictionaryScript.cs
+++ Assets/Scripts/DictionaryScript.cs
@@ -1,6 +1,68 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class DictionaryScript : MonoBehaviour
+public class DictionaryScript : MonoBehaviour, ISerializationCallbackReceiver
 {
-    
+    [SerializeField]
+    private DictionaryScriptableObject dictionaryScriptableObject;
+
+    [Header("DICTIONARY")]
+        [SerializeField]
+        private List<string> keys = new List<string>();
+        [SerializeField]
+        private List<int> values = new List<int>();
+
+    private Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+    public bool modifyValues;
+
+    private void Awake()
+    {
+        for (int i = 0; i < Mathf.Min(dictionaryScriptableObject.Keys.Count, dictionaryScriptableObject.Values.Count); i++)
+        {
+            myDictionary.Add(dictionaryScriptableObject.Keys[i], dictionaryScriptableObject.Values[i]);
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+        if(modifyValues == false)
+        {
+            keys.Clear();
+            values.Clear();
+            for(int i=0;i<Mathf.Min(dictionaryScriptableObject.Keys.Count, dictionaryScriptableObject.Values.Count); i++)
+            {
+                keys.Add(dictionaryScriptableObject.Keys[i]);
+                values.Add(dictionaryScriptableObject.Values[i]);
+            }
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+    }
+
+    public void DeserializeDictionary()
+    {
+        Debug.Log("DESERIALIZATION");
+        myDictionary = new Dictionary<string, int>();
+        dictionaryScriptableObject.Keys.Clear();
+        dictionaryScriptableObject.Values.Clear();
+        for (int i = 0; i < Mathf.Min(keys.Count, values.Count); i++)
+        {
+            dictionaryScriptableObject.Keys.Add(keys[i]);
+            dictionaryScriptableObject.Values.Add(values[i]);
+            myDictionary.Add(keys[i], values[i]);
+        }
+        modifyValues = false;
+    }
+
+    public void PrintDictionary()
+    {
+        foreach(var pair in myDictionary)
+        {
+            Debug.Log("Key: " + pair.Key + " Value: " + pair.Value);
+        }
+    }
+
 }
