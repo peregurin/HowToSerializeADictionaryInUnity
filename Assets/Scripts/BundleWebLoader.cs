b8ae303eb3b34fa070a26ebf91bd7ab02a600a36
peregurin <chinmayvirmani@gmail.com>
2021-02-13 16:54:53 +0530

    Asset bundles implementation
    created an asset bundle, moved it to drive and then downloaded it and instantiated it in the game.

---
+++ Assets/Scripts/BundleWebLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BundleWebLoader : MonoBehaviour
+{
+    public string bundleUrl = "https://drive.google.com/uc?export=download&id=12VjWlIRiSpT4bakK3X-oh-iuEMzb_1vL";
+    public string assetName = "BundledObject";
+
+    IEnumerator Start()
+    {
+        using (UnityWebRequest web = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl))
+        {
+            yield return web.SendWebRequest();
+            if (web.isNetworkError || web.isHttpError)
+            {
+                Debug.Log(web.error);
+            }
+            else
+            {
+                AssetBundle remoteAssetBundle = DownloadHandlerAssetBundle.GetContent(web);
+                if (remoteAssetBundle == null)
+                {
+                    Debug.LogError("Failed to download AssetBundle!");
+                    yield break;
+                }
+                Instantiate(remoteAssetBundle.LoadAsset(assetName));
+                remoteAssetBundle.Unload(false);
+            }          
+        }
+    }
+}
